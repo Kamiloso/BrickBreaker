e0ed172d7fc729d6ea4f108e7032ba1977ec342f
Kamil <kamiloso.skoposo@gmail.com>
2025-03-14 19:41:04 +0100

    test 9

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "testfile.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Czeœæ trochê tod zmieni³em!" << endl;
+    cout << "Czeœæ trochê todo zmieni³em!" << endl;
     for (int i = 0; i < 6; i++)
         cout << "Po co sie ograniczac? Po nic!!!" << endl;
+    cout << yuo() << endl;
 
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
     sf::CircleShape shape(100.f);
---
+++ testfile.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int yuo()
+{
+	return 1;
+}
