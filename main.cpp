b638acaf38c8dacc2814e8b17e84541226e436de
JulianGranieczny <julian.granieczny@gmail.com>
2025-03-14 19:08:31 +0100

    test 3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Czeœæ trochê to zmieni³em!" << endl;
-    cout << "Witaj, odpowiadam ci!" << endl;
 
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
     sf::CircleShape shape(100.f);
