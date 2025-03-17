aa718fde6c36521bedef8cd440585cdbfae2df27
JulianGranieczny <julian.granieczny@gmail.com>
2025-03-17 22:02:48 +0100

    Zmiany okna
    Rzeczy dodane:
    -Okno odpala się w trybie pełnoekranowym
    -Można zmieniać tryb pełnoekranowy
    -Można zmienić wyświetlaną rozdzielczość w oknie jak i trybie pełnoekranowym
    -Nie można ręcznie zmieniać rozmiarów okna
    Czego nie udało się zrobić:
    -Czarne paski po bokach przy trybie pełnoekranowym(SFML nie ma jakiejś swojej metody na to. Chyba by trzeba było zrobić jakąś metodę która rysuje paski czarne, ale nwm kiedy by ona wiedziała że musi je rysować albo gdzie.)
    -ukrywanie konsoli(z tego co czytałem to jest to rzecz powiązana bardziej z windowsem niż C++, więc na razie nie robiłem tego, bo popsuło by to kompatybilność z Linuxem najprawdopodobniej.)

--- main.cpp
+++ main.cpp
@@ -1,35 +1,23 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
-#include "testfile.h"
-
+#include "GameWindow.h"
 using namespace std;
+/*
+F-prze³¹czanie trybu pe³noekranowgo
+1-rozdzielczoœæ 1280x720
+2-rozdzielczoœæ 1600x900
+3-rozdzielczoœæ 1920x1080
+ESC-wyjœcie z programu
+
+*/
+
+
 
 int main()
 {
-    cout << "Czeœæ trochê todo zmieni³em!" << endl;
-    for (int i = 0; i < 5; i++)
-        cout << "Po co sie ograniczac? Po nic!!!" << endl;
-    cout << yuo() << endl;
 
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    GameWindow GameWindow(1,"test"); //Stworzenie obiektu okna
+    GameWindow.run(); //Pêtla gry
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    system("pause");
 
     return 0;
 }
