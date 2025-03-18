202121d4004f0ccbf15211a153a3349967a8aa97
Kamil <kamiloso.skoposo@gmail.com>
2025-03-18 17:27:54 +0100

    Revert "very minor changes 1"
    This reverts commit a1734bb1fdf73d9c52b19b66be1e42acc5db1ca3.

--- GameWindow.h
+++ GameWindow.h
@@ -8,6 +8,14 @@ using namespace std;
 
 class GameWindow //Klasa okna gry
 {
+public:
+	GameWindow(int resolution,string title); //Konstruktor, przyjmuje rozdzielczoœæ i tytu³ okna
+
+	void run(); //Funkcja odpowiedzialna za g³ówn¹ pêtlê gry
+	void centerWindow(); //Funkcja zmieniaj¹ca pozycjê okna na centrum ekranu
+	void drawCircle(); //Funkcja rysuj¹ca obiekt wykorzystywana w ramach testowania zmiany rozdzielczoœci
+	void setView(); //Ustawianie widoku(rozdzielczoœæ jaka jest wyœwietlana) ale nie zmienianie rozmiarów okna
+
 private:
 	// wektor rozmiarów ekranu jaki komputer mo¿e przyj¹æ w trybie pe³noekranowym,
 	// s³u¿y do ustalania rozdzielczoœci przy tworzeniu okna i ustalaniu rozdzielczoœci przy trybie pe³noekranowym.
@@ -18,9 +26,9 @@ private:
 	// wektor mo¿liwych rozdzielczoœci ekranu,
 	// s³u¿y do zmiany rozdzielczoœci podczas dzia³ania programu.
 	vector<sf::Vector2u> resolutions = {
-		{1280,720},
-		{1600,900},
-		{1920,1080}
+	{1280,720},
+	{1600,900},
+	{1920,1080}
 	};
 	// zmienna która zapisuje jaka jest aktualnie wybrana rozdzielczoœæ. Reprezentuje indeks rozdzielczoœci z wektora resolutions,
 	// u¿ywana przy zmianie rozdzielczoœci i przejœciu z trybu pe³noekranowego na tryb w oknie.
@@ -32,13 +40,5 @@ private:
 	void eventHandling(); //Funkcja odpowiedzialna za rozpatrywanie ró¿nych zdarzeñ
 	void update(); //Funkcja odpowiedzialna za aktualizowanie stanu gry
 	void render(); //Funkcja odpowiedzialna za wyœwietlanie aktualnego stanu gry
-
-public:
-	GameWindow(int resolution, string title); //Konstruktor, przyjmuje rozdzielczoœæ i tytu³ okna
-
-	void run(); //Funkcja odpowiedzialna za g³ówn¹ pêtlê gry
-	void centerWindow(); //Funkcja zmieniaj¹ca pozycjê okna na centrum ekranu
-	void drawCircle(); //Funkcja rysuj¹ca obiekt wykorzystywana w ramach testowania zmiany rozdzielczoœci
-	void setView(); //Ustawianie widoku(rozdzielczoœæ jaka jest wyœwietlana) ale nie zmienianie rozmiarów okna
 };
 
