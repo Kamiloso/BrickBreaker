6cbd67c7b2581918083ee24512c1f1292653c580
Kamil <kamiloso.skoposo@gmail.com>
2025-04-08 16:56:38 +0200

    gitignore + star e^sinx

--- Star.cpp
+++ Star.cpp
@@ -5,7 +5,7 @@
 Star::Star(float _x, float _y, float _r, mt19937* _gen, int _layer) : SceneObject(_x, _y, _layer), r(_r), gen(_gen) {
 	uniform_real_distribution<> distFirstOpacityPhase(0, 255);
 	opacity_time = distFirstOpacityPhase(*gen);
-	uniform_real_distribution<> distPhase(0.5, 0.7);
+	uniform_real_distribution<> distPhase(0.8, 2);
 	phase = distPhase(*gen);
 }
 
@@ -16,7 +16,7 @@ void Star::draw(GameWindow* game_window) {
 void Star::lateUpdate(float delta_time)
 {
 	opacity_time += delta_time;
-	float opacity = (255.0 / 2.0) * (sin(phase * (opacity_time)) + 1);
-	//float opacity = (255.0 / 2.0) * (-1 / 4 * pow(2.84, 2 * sin(opacity_time * phase)) + 2.017);
+	//float opacity = (255.0 / 2.0) * (sin(phase * (opacity_time)) + 1);
+	float opacity = (255.0 / 2.0) * (-1.0 / 4.0 * pow(2.84, 2 * sin(opacity_time * phase)) + 2.017);
 	color = sf::Color(255, 255, 153, opacity);
 }
